2640005503e8a7c0ee7244ea56193be461b8926a
Smony <dev.symonchuk@gmail.com>
2017-11-03 16:56:35 +0200

    add Rectangl random

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import core.Window;
 import sandbox.Example;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args)
     {
         Window window = new Window("Smony", 960, 640);
-        window.addLayer(new Example());
+        window.addLayer(new Example("Test", Color.BLUE));
+        window.addLayer(new Example("Test2", Color.GREEN));
     }
 }
--- sandbox/Example.java
+++ sandbox/Example.java
@@ -7,13 +7,42 @@ import events.types.MousePressedEvent;
 import events.types.MouseReleasedEvent;
 import layers.Layer;
 
+import java.awt.*;
+import java.util.Random;
+
+import static java.awt.Color.*;
+
 public class Example extends Layer {
+
+    private String name;
+    private Color color;
+    private Rectangle box;
+
+    private static final Random rand = new Random();
+
+    public Example(String name, Color color)
+    {
+        this.name = name;
+        this.color = color;
+
+        this.box = new Rectangle(rand.nextInt(100) + 150, rand.nextInt(100) + 150, 120, 240);
+    }
+
     public void onEvent(Event event)
     {
         Dispatcher dispatcher = new Dispatcher(event);
         dispatcher.dispath(Event.Type.MOUSE_PRESSED, (Event e) -> onPressed((MousePressedEvent)e));
         dispatcher.dispath(Event.Type.MOUSE_RELEASED, (Event e) -> ondReleased((MouseReleasedEvent)e));
         dispatcher.dispath(Event.Type.MOUSE_MOVED, (Event e) -> onMoved((MouseMotionEvent)e));
+    }
+
+    public void onRender(Graphics g)
+    {
+        g.setColor(color);
+        g.fillRect(box.x, box.y, box.width, box.height);
+
+        g.setColor(Color.WHITE);
+        g.drawString(name, box.x + 10, box.y + 10);
     }
 
     private boolean onPressed(MousePressedEvent event) {
